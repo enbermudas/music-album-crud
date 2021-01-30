7b1560aee75f91dae46fd98ccdf39f4ba1eb5a5d
Enrique Bermúdez <enbermudez96@gmail.com>
2021-01-30 18:15:05 -0300

    fix: update model loading

--- src/server/models/index.js
+++ src/server/models/index.js
@@ -1,25 +1,19 @@
 import Sequelize from 'sequelize';
-import path from 'path';
-import fs from 'fs';
 import config from '../../../db.config.json';
-
-const basename = path.basename(__filename);
+import album from './album';
+import artist from './artist';
+import song from './song';
 
 const env = process.env.NODE_ENV || 'dev';
 const { user, password, database } = config[env];
 
 const db = {};
 
-const sequelize = new Sequelize(database, user, password, config);
+const sequelize = new Sequelize(database, user, password, config[env]);
 
-fs.readdirSync(__dirname)
-  .filter((file) => {
-    return file.indexOf('.') !== 0 && file !== basename && file.slice(-3) === '.js';
-  })
-  .forEach((file) => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
+db.Album = album(sequelize, Sequelize.DataTypes);
+db.Artist = artist(sequelize, Sequelize.DataTypes);
+db.Song = song(sequelize, Sequelize.DataTypes);
 
 Object.keys(db).forEach((modelName) => {
   if (db[modelName].associate) {
