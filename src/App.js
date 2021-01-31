e96c35a9482aa08058c12cb18108e78b54409caf
Enrique Bermúdez <enbermudez96@gmail.com>
2021-01-31 14:20:53 -0300

    feat: create pages skeleton

--- src/App.js
+++ src/App.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Albums from './pages/Albums';
+import Artists from './pages/Artists';
+import Songs from './pages/Songs';
+import './App.css';
 
 const App = () => {
-  return <div className="App">App</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+
+        <Switch>
+          <Route exact path={['/', '/artists']}>
+            <Artists />
+          </Route>
+
+          <Route exact path="/albums">
+            <Albums />
+          </Route>
+
+          <Route exact path="/songs">
+            <Songs />
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
 };
 
 export default App;
