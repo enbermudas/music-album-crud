8e2e2d859edb3a19c59b2da5dc4d533fbf8b1d75
Enrique Bermúdez <enbermudez96@gmail.com>
2021-02-01 20:39:40 -0300

    style: lint code

--- src/server/services/artist.js
+++ src/server/services/artist.js
@@ -22,7 +22,7 @@ const createArtist = async (req, res) => {
 
 const getArtists = async () => {
   const artists = await Artist.findAll({
-    order: [['createdAt', 'DESC']],
+    order: [['createdAt', 'DESC']]
   });
 
   return artists;
